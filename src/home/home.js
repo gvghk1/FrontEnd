6eec3e19fd771e17769a4a372dd7da987b9fc82d
Help <pczerokey@gmail.com>
2019-09-20 08:46:00 -0400

    trying to create homepage

---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GeekText</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
