c25b7fddcfb0bca4c1218d3cea83ca3de83e5530
Help <pczerokey@gmail.com>
2019-10-03 19:40:26 -0400

    package updates, and styling

--- src/components/Bookitem.js
+++ src/components/Bookitem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 
 export class Bookitem extends Component {
+
+    style= {"width": "18rem"}
     render() {
         return (
-            <div>
-                <p>Book Name {this.props.book.bookname}</p>
-                <p>Author Name {this.props.book.authorName}</p>
-                <p>Author Bio {this.props.book.authorBio}</p>
+
+            <div className="card" style={this.style}>
+                <div className="card-body">
+                    <h5 className="card-title">Book Name: {this.props.book.bookname}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Author Bio: {this.props.book.authorBio}</h6>
+                    <p className="card-text">Author Name: {this.props.book.authorName}</p>
+                </div>
             </div>
+
         )
     }
 }
