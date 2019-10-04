c1b6816ff53987bfd200946a3f642d4f7f3ad9f4
Help <pczerokey@gmail.com>
2019-10-04 19:41:01 -0400

    info now being fetched

--- src/App.js
+++ src/App.js
@@ -12,15 +12,21 @@ class App extends React.Component {
       bookdetails: 
         [
             {
-                id: 1,
-                bookname: 'Courage Mountain',
-                authorName: 'Ferguson Yeomans',
-                authorBio: 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.',
-                bookDescrip: 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
-                bookGenre: 'Adventure|Children|Drama',
-                book_pub: 'Dotted Hawthorn',
-                book_rel: '9/12/2018',
-                bookRate: '5',
+                "_id":"5d97808aeec2e9b7d414ce5a",
+                "id":33,
+                "book_name":"Kind Hearts and Coronets",
+                "book_cover":"http://dummyimage.com/350x350.png/ff4444/ffffff",
+                "author_first_name":"Harvey",
+                "author_last_name":"Inskipp",
+                "author_biography":"Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+                "book_desc":"Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+                "book_genre":"Comedy|Drama","book_publisher":"Eastern Milkpea","book_releaseDate":"06/08/2019",
+                "book_rating":4,
+                "email":"hinskippw@discuz.net",
+                "gender":"Male",
+                "book_publishing_info":"6/22/2019",
+                "book_copies_sold":95,
+                "book_price":39
             },
             {
                 id: 2,
@@ -42,10 +48,13 @@ class App extends React.Component {
         textAlign: "center",
     };
 
+    setState
+
     componentDidMount(){
         console.log("First Run")
         this.getData();
         console.log("Last Run")
+        console.log('after change?', this.state.bookdetails)
     }
 
     // componentWillMount()
@@ -62,6 +71,8 @@ class App extends React.Component {
           console.log(response)
           console.log('hoping for data', response.data)
           this.state.bookdetails = response.data
+          //here should change?
+          this.setState({bookdetails: response.data});
           console.log("Set Run")
         } catch (error) {
           console.error(error);
