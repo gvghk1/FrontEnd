4f4f1dfb417ec323efe4df5303fc683fd8790172
Help <pczerokey@gmail.com>
2019-10-04 19:43:56 -0400

    info now being displayed

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -8,9 +8,9 @@ export class Bookitem extends Component {
 
             <div className="card" style={this.style}>
                 <div className="card-body">
-                    <h5 className="card-title">Book Name: {this.props.book.bookname}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Author Bio: {this.props.book.authorBio}</h6>
-                    <p className="card-text">Author Name: {this.props.book.authorName}</p>
+                    <h5 className="card-title">Book Name: {this.props.book.book_name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Author Bio: {this.props.book.author_biography}</h6>
+                    <p className="card-text">Author Name: {this.props.book.author_first_name + " " + this.props.book.author_last_name}</p>
                 </div>
             </div>
 
