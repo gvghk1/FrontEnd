3b827170cd83a2205193fc0a4157ecb86e80ce0d
timmarr98 <tmarr006@fiu.edu>
2019-10-08 11:45:43 -0400

    added publishing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const url = 'https://geek-text-backend.herokuapp.com/api';
 class App extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props); 
         this.state = {
             bookdetails:
                 [
