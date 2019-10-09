6dfcfe9756b038de8e82f0d3797529ace6ca95d0
Help <pczerokey@gmail.com>
2019-10-09 08:08:18 -0400

    added timmy's new changes

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -14,6 +14,13 @@ export class Bookitem extends Component {
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Book Name: {this.props.book.book_name}</h5>
+          <br></br><img src={this.props.book.book_cover} alt="bookcover placeholder" width="100" height="100"></img>
+          <h7 className ="card-subtitle mb-2 text-muted">Publish Date: {this.props.book.book_publishing_info + " "}</h7>
+            <h8 className = "card-subtitle mb-2 text-muted">Release Date:{this.props.book.book_releaseDate} </h8>
+            <br></br><h9 className = "card-subtitle mb-2 text-muted">Genre: {this.props.book.book_genre}  </h9>
+            <br></br><h9 class name = "card-subtitle mb-2 text-muted">Rating: {this.props.book.book_rating}</h9>
+            <br></br><h10 className = "card-text"> Publisher: {this.props.book.book_publisher}</h10>
+            <p className="card-text">Author Name: {this.props.book.author_first_name + " " + this.props.book.author_last_name + " "}</p>
           <h6 className="card-subtitle mb-2 text-muted">
             Author Bio: {this.props.book.author_biography}
           </h6>
