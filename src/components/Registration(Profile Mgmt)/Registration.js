917be90b668bd9d14afcb235be97038f82c77e20
Help <pczerokey@gmail.com>
2019-10-10 10:57:09 -0400

    added a workspace for paul to work and see - profile

---
+++ src/components/Registration(Profile Mgmt)/Registration.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Registration extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <h1>Paul's Workspace - Martyn</h1>
+      </div>
+    );
+  }
+}
+
+export default Registration;
