e2f0f5503c7cf3180dedbab046608d00a513b88d
Help <pczerokey@gmail.com>
2019-10-15 11:36:30 -0400

    DISPLAY where filter section would show

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -6,7 +6,7 @@ class Filter extends Component {
     render() {
         return (
             <div className="">
-                <h1>Filter Section - Martyn</h1>
+                <h1>Filter Section HERE</h1>
             </div>
         );
     }
