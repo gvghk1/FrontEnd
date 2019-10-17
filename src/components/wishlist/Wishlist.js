cd47ed894f46315d564ed0c70823775ddf0ec6bc
EstebanH <ehuez002@fiu.edu>
2019-10-17 11:57:45 -0400

    With render

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-class Wishlist extends React.Component {}
+class Wishlist extends Component {
+  clickRemove = id => {
+    this.props.clickRemove(id);
+  };
 
-export default Wishlist;
+  render() {
+    let addedItemID = this.props.items.length ? (
+      this.props.items.map(item => {
+        return (
+          <div className="slot" key={item.id}>
+            <Link to="/#Items">
+              <a href="#cart" class="links" type="button">
+                Back to Home Page
+              </a>
+            </Link>
+          </div>
+        );
+      })
+    ) : (
+      <div>
+        <p>This page occurs if nothing is here.</p>
+      </div>
+    );
+    return (
+      <div className="container">
+        <div className="#cart">
+          <h4>Wishlist:</h4>
+          <ul className="current-items">{addedItemID}</ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+const currentItems = state => {
+  return {
+    items: state.addedItemID,
+    total: state.total
+  };
+};
+
+export default connect(currentItems)(Wishlist);
